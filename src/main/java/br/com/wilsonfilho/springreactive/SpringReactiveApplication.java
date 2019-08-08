2c6d7ce74267b63229bd99c059d3030d77bbe60f
Wilson Luiz <wilson.luiz@solutis.com.br>
2019-08-08 10:33:14 -0300

    Commit initial

---
+++ src/main/java/br/com/wilsonfilho/springreactive/SpringReactiveApplication.java
@@ -0,0 +1,13 @@
+package br.com.wilsonfilho.springreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveApplication.class, args);
+	}
+
+}
